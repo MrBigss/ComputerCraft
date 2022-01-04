f5a91cd9740250f4e444f7aa4183bd948c7d9d07
MrBigs <84638013+MrBigss@users.noreply.github.com>
2022-01-04 18:30:18 -0500

    Create setup.lua

---
+++ setup.lua
@@ -0,0 +1,5 @@
+-- Put on a floopy disk, and rename as startup.lua, in root.
+-- Then when put in a drive next to a turtle, this will set it up, and/or update it.
+
+shell.run("pastebin run p8PJVxC4")
+shell.run("github clone MrBigss/ComputerCraft -b main")
